471deeed019c743a563365c09624934ee55d33ee
Abubakar Malik <abub81159@gmail.com>
2024-11-30 15:42:30 +0500

    updated and addition

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,8 +2,8 @@ import express, { Request, Response, NextFunction } from "express";
 import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import helmet from "helmet";
-import cors from "cors";
 import path from "path";
+import cors from "cors";
 
 // Import Routes
 import userRoute from "./routes/userRoutes";
@@ -18,12 +18,11 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(helmet()); // Basic security headers
 
-// CORS - Configure for your frontend domain in production
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow only frontend to make request
-    methods: ["GET", "POST", "PUT", "DELETE"], // Define allowed methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Define allowed headers
+    origin: "https://diary-app-one.vercel.app/",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
