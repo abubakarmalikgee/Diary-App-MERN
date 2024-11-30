fa2588a633193a54f5587fc881b5b37061347aa0
Abubakar Malik <165191310+abubakarmalikgee@users.noreply.github.com>
2024-11-30 19:41:46 +0500

    Delete server/dist directory

--- server/dist/middlewares/catchAsyncErrors.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const asyncHandler = (theFunc) => {
-    return (req, res, next) => {
-        Promise.resolve(theFunc(req, res, next)).catch(next);
-    };
-};
-exports.default = asyncHandler;
